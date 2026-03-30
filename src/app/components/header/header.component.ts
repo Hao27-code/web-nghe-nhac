7fe5b9cc43d1f83292ff6c227789e41ba1cc7f61
Hao27-code <https://github.com/Hao27-code/web-nghe-nhac.git>
2026-03-31 00:28:04 +0700

    init project

---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import {IonicModule} from "@ionic/angular";
+import {Router} from "@angular/router";
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss'],
+  imports: [
+    IonicModule
+  ]
+})
+export class HeaderComponent   {
+
+  // eslint-disable-next-line @angular-eslint/prefer-inject
+  constructor(private router:Router) { }
+
+
+
+  goSearch() {
+    this.router.navigate(['/search']);
+  }
+}
---
+++ src/app/search/search.page.ts
@@ -0,0 +1,30 @@
+import {IonicModule} from "@ionic/angular";
+import {Component} from "@angular/core";
+import { Location } from '@angular/common';
+
+
+@Component({
+  selector: 'app-search',
+  templateUrl: './search.page.html',
+  styleUrls: ['./search.page.scss'],
+  standalone: true,
+  imports: [ IonicModule]
+})
+
+export class SearchPage  {
+  private router: any;
+
+  // eslint-disable-next-line @angular-eslint/prefer-inject
+  // @ts-ignore
+  // eslint-disable-next-line @angular-eslint/prefer-inject
+  constructor(private location: Location) { }
+
+  //Quay về trang trước
+  goBack() {
+    this.location.back();
+  }
+
+  protected onSearch($event: any) {
+
+  }
+}
