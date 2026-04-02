0cc69211bb9afa9e44504c468d1a774464272867
Hao27-code <nguyenanhhao2701206@gmail.com>
2026-04-02 15:15:38 +0700

    update home banner

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, ElementRef, ViewChild} from '@angular/core';
 import {HeaderComponent} from "../components/header/header.component";
 import {IonicModule} from "@ionic/angular";
 import {NgClass, NgOptimizedImage} from "@angular/common";
@@ -8,8 +8,11 @@ import {NgClass, NgOptimizedImage} from "@angular/common";
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   imports: [HeaderComponent, IonicModule, NgClass, NgOptimizedImage],
+
 })
 export class HomePage {
+
+  currentIndex: number = 0;
   constructor() {}
   greeting='';
   ngOnInit() {
@@ -38,4 +41,45 @@ export class HomePage {
     }
   ];
 
+  loopPlaylists = [
+    ...this.playlists,
+    this.playlists[0] // clone ảnh đầu
+  ];
+  @ViewChild('scrollContainer', { static: false }) scrollContainer!: ElementRef;
+  ngAfterViewInit(): void {
+
+    setInterval(() => {
+
+      const el = this.scrollContainer.nativeElement;
+      const total = this.playlists.length;
+      this.currentIndex++;
+
+      el.scrollTo({
+        left: el.clientWidth * this.currentIndex,
+        behavior: 'smooth'
+      });
+
+      // 👉 khi tới slide clone
+      if (this.currentIndex === total) {
+
+        setTimeout(() => {
+
+          el.style.scrollBehavior = 'auto'; // 🔥 tắt animation
+
+          el.scrollLeft = 0;
+
+          this.currentIndex = 0;
+
+          setTimeout(() => {
+            el.style.scrollBehavior = 'smooth'; // bật lại
+          }, 50);
+
+        }, 300);
+
+      }
+
+
+    }, 3000);
+
+  }
 }
